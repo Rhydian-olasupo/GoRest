afa4446003e594965b886f174acae3a9e3570aa8
Olasupo <alfredolasupo@gmail.com>
2024-02-20 18:32:35 +0000

    GORM refactored

--- chapter7/jsonstore/helper/models.go
+++ chapter7/jsonstore/helper/models.go
@@ -8,14 +8,16 @@ import (
 
 type Shipment struct {
 	gorm.Model
-	Packages []Package `gorm:"foreignKey:ShipmentID"` // Define the one-to-many relationship
-	Data     string    `sql:"type:JSONB NOT NULL DEFAULT '{}'::JSONB" json:"-"`
+	//ShipmentID uint64
+	Packages []Package /*`gorm:"foreignKey:ShipmentID"`*/ // Define the one-to-many relationship
+	Data     string                                       `sql:"type:JSONB NOT NULL DEFAULT '{}'::JSONB" json:"-"`
 }
 
 type Package struct {
 	gorm.Model
-	ShipmentID uint   // Foreign key for the Shipment
-	Data       string `sql:"type:JSONB NOT NULL DEFAULT '{}'::JSONB"`
+	//ShipmentID uint // Foreign key for the Shipment
+	//Shipment   *Shipment `gorm:"foreignKey:ShipmentID;belongsTo"` // Reverse relationship
+	Data string `sql:"type:JSONB NOT NULL DEFAULT '{}'::JSONB"`
 }
 
 //GORM creates tables with plural names
@@ -31,7 +33,7 @@ func (Package) TableName() string {
 
 func InitDB() (*gorm.DB, error) {
 	var err error
-	dsn := "host=localhost user=postgres password=mofito dbname=mydb port=5432 sslmode=disable"
+	dsn := "host=localhost user=**** password=**** dbname=mydb port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn))
 	if err != nil {
 		return nil, err
