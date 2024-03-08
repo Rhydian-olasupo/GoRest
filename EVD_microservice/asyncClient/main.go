815a048eaa2d113724ad1cb27ddc2ff7a3022f33
Olasupo <alfredolasupo@gmail.com>
2024-03-08 21:51:13 +0000

    Publish and Subscribe

---
+++ EVD_microservice/asyncClient/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	proto "go_trial/gorest/EVD_microservice/asyncClient/protofiles"
+
+	micro "go-micro.dev/v4"
+)
+
+// ProcessEvent processes a weather alert
+func ProcessEvent(ctx context.Context, event *proto.Event) error {
+	log.Println("Got alert:", event)
+	return nil
+}
+
+func main() {
+	//Create a new service
+	service := micro.NewService(micro.Name("weather-alert"))
+
+	//Initialize the client and parse the command line flags
+
+	service.Init()
+
+	micro.RegisterSubscriber("alerts", service.Server(), ProcessEvent)
+
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
---
+++ EVD_microservice/asyncServ/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	proto "go_trial/gorest/EVD_microservice/asyncServ/protofiles"
+
+	micro "go-micro.dev/v4"
+)
+
+func main() {
+	//Create a new service. Optionally add options here.
+
+	service := micro.NewService(
+		micro.Name("weather"),
+	)
+
+	p := micro.NewEvent("alerts", service.Client())
+
+	go func() {
+		for now := range time.Tick(15 * time.Second) {
+			log.Println("Publishing weather alert to Topic: alerts")
+			p.Publish(context.TODO(), &proto.Event{
+				City:        "Munich",
+				Timestamp:   now.UTC().Unix(),
+				Temperature: 2,
+			})
+
+		}
+
+	}()
+
+	//Run Server
+	if err := service.Run(); err != nil {
+		log.Println(err)
+	}
+
+}
