af5fb234fdd21d55ed373cdc2c10320dbcaa9f8d
Olasupo <alfredolasupo@gmail.com>
2024-03-08 16:04:01 +0000

    encrypttion Service Tidied

--- microservice/encryptService/handlers.go
+++ microservice/encryptService/handlers.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"context"
+
+	proto "go_trial/gorest/microservice/encryptService/proto"
+)
+
 //Encrypter Holds the information about the methods
 
 type Encrypter struct{}
 
 //Encrypt converst a message into cipher and returns response
+
+f
